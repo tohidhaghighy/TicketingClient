716c68f3d5dd6dcba6f38fb2b72446f591100d0f
[Ehsan-Darvishi] <ehsan20031203@gmail.com>
2024-11-11 14:10:03 +0330

    1403/08/21 fixing search

--- components/chart/search/searching.vue
+++ components/chart/search/searching.vue
@@ -139,9 +139,104 @@
   
   <div class="row">
     <div class="col-md-12">
-      <ClientOnly>
-        <ChartSearchList :data="searchResult"/>
-      </ClientOnly>
+      <div class="card-body">
+        <table id="ticketlist" class="table table-striped table-bordered" width="100%">
+        <thead>
+          <tr>
+            <th>شماره ردیف تیکت</th>
+						<th>کد تیکت</th>
+						<th>ثبت کننده</th>
+						<th>تاریخ ثبت</th>
+						<th>نوع درخواست </th>
+						<th>اولویت</th>
+						<th>عنوان</th>
+						<th>سامانه</th>
+						<th >ایجاد شده توسط</th>
+						<th>ارجاع شده به</th>
+						<th>وضعیت</th>
+						<th>انجام دهنده</th>
+						<th>ساعت صرف شده</th>
+						<th>جزئیات</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in searchResult" :key="item.id">
+            <td>{{item.ticketRowNumber}} </td>
+							<td>{{item.ticketNumber}} </td>
+							<td>{{item.username}} </td>
+							<td>{{item.date}}</td>
+							<td v-if="item.requestType==1">پشتیبانی</td>
+							<td v-else-if="item.requestType==2">توسعه</td>
+							<td v-else></td>
+							<td v-if="item.priority==1">بالا</td>
+							<td v-else-if="item.priority==2">متوسط</td>
+							<td v-else>پایین</td>
+							<td>{{item.title}}</td>
+							<td>{{item.project}}</td>
+							<td>{{item.insertedRoleId}}</td>
+              <td>{{item.currentRoleId}}</td>
+							<td v-if="item.statusId==1">
+								<p class="text-success">{{ item.status }}</p>
+							</td>
+							<td v-else-if="item.statusId==2">
+								<a class="text-primary">{{ item.status }}</a>
+							</td>
+							<td v-else-if="item.statusId==3">
+								<a class="text-warning">{{ item.status }}</a>
+							</td>
+							<td v-else-if="item.statusId==4">
+								<a class="text-danger">{{ item.status }}</a>
+							</td>
+							<td v-else-if="item.statusId==5">
+								<a class="text-warning">{{ item.status }}</a>
+							</td>
+							<td v-else-if="item.statusId==6">
+								<a class="text-primary">{{ item.status }}</a>
+							</td>
+							<td v-else-if="item.statusId==7">
+								<a class="text-warning">{{ item.status }}</a>
+							</td>
+							<td v-else-if="item.statusId==8">
+								<a class="text-primary">{{ item.status }}</a>
+							</td>
+							<td v-if="item.developerId==1">
+								<p>پویا رضاییه</p>
+							</td>
+							<td v-else-if="item.developerId==2">
+								<p>محمد باقری</p>
+							</td>
+							<td v-else-if="item.developerId==3">
+								<p>توحید حقیقی</p>
+							</td>
+							<td v-else-if="item.developerId==4">
+								<p>مهسا برجی</p>
+							</td>
+							<td v-else-if="item.developerId==5">
+								<p>امیر مسعود صالحی</p>
+							</td>
+							<td v-else-if="item.developerId==6">
+								<p>شکیلا کاظم پور</p>
+							</td>
+							<td v-else-if="item.developerId==7">
+								<p>احسان درویشی</p>
+							</td>
+							<td v-else-if="item.developerId==8">
+								<p>الهه ابراهیمی</p>
+							</td>
+							<td v-else-if="item.developerId==9">
+								<p>ساناز محمد زاده</p>
+							</td>
+							<td v-else-if="item.developerId==10">
+								<p> تخصیص نشده</p>
+							</td>
+							<td>{{item.ticketTime}}</td>
+							<td>
+								<nuxt-link :to="{ path: '/ticket/detail', query: {id: item.id}}">مشاهده</nuxt-link>
+							</td>
+          </tr>
+        </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -151,49 +246,61 @@
   
   const user = useCookie("UserInfo");
   const { public: { ticketingUrl }} = useRuntimeConfig();
-  const searchResult = reactive([]);
   const query = reactive({
-  TicketNumber:null,
-  Title:null,
-  InsertedRoleId:0,
-  Username:null,
-  CurrentRoleId:0,
-  StatusId:0,
-	ProjectId:0,
-  RequestType:0,
-  DeveloperId:0,
+    TicketNumber: "",
+    Title: "",
+    InsertedRoleId: 0,
+    Username: "",
+    CurrentRoleId: 0,
+    StatusId: 0,
+    ProjectId: 0,
+    RequestType: 0,
+    DeveloperId: 0,
 });
   
   onMounted(() => {
     $('input[name="date-picker-shamsi-list"]').datepicker({
-          dateFormat: "yy/mm/dd",
-          showOtherMonths: true,
-          selectOtherMonths: true,
-          changeMonth: true,
-          changeYear: true,
-          showButtonPanel: true
-      });
+      dateFormat: "yy/mm/dd",
+      showOtherMonths: true,
+      selectOtherMonths: true,
+      changeMonth: true,
+      changeYear: true,
+      showButtonPanel: true
+    });
   })
   
   function convertShamsiToGregorian(shamsiDate) {
-      // فرض کنید تاریخ ورودی به فرمت "yyyy/MM/dd" باشد
-      const [year, month, day] = shamsiDate.split('/').map(Number);
-      const { gy, gm, gd } = jalaali.toGregorian(year, month, day);
-      
-      return `${gy}-${String(gm).padStart(2, '0')}-${String(gd).padStart(2, '0')}`;
+    // فرض کنید تاریخ ورودی به فرمت "yyyy/MM/dd" باشد
+    const [year, month, day] = shamsiDate.split('/').map(Number);
+    const { gy, gm, gd } = jalaali.toGregorian(year, month, day);
+    
+    return `${gy}-${String(gm).padStart(2, '0')}-${String(gd).padStart(2, '0')}`;
   }
-  
+
+  const searchResult = ref([]);
+
   async function send(){
     var StartDateTime = document.getElementById('startDateTime').value;
     var EndDateTime = document.getElementById('endDateTime').value;
-    if(StartDateTime !=null && EndDateTime != null)
+    if (!StartDateTime || !EndDateTime) 
     {
-      const { data : searchResult , error } = await useFetch(`${ticketingUrl}/api/v1/search?ticketNumber=${query.TicketNumber}&title=${query.Title}&insertedRoleId=${query.InsertedRoleId}&username=${query.Username}&CurrentRoleId=${query.CurrentRoleId}&statusId=${query.StatusId}&projectId=${query.ProjectId}&requestType=${query.RequestType}&developerId=${query.DeveloperId}&startDateTime=${convertShamsiToGregorian(StartDateTime)}&endDateTime=${convertShamsiToGregorian(EndDateTime)}`);
+    toastr.error('لطفا تاریخ شروع و پایان را مشخص کنید');
     }
-    else
-    {
-      toastr.error('لطفا تاریخ شروع و پایان را مشخص کنید');
+    try {
+    const { data, error: fetchError } = await useFetch(
+      `${ticketingUrl}/api/v1/search?ticketNumber=${query.TicketNumber}&title=${query.Title}&insertedRoleId=${query.InsertedRoleId}&username=${query.Username}&CurrentRoleId=${query.CurrentRoleId}&statusId=${query.StatusId}&projectId=${query.ProjectId}&requestType=${query.RequestType}&developerId=${query.DeveloperId}&startDateTime=${convertShamsiToGregorian(StartDateTime)}&endDateTime=${convertShamsiToGregorian(EndDateTime)}`
+    );
+
+    if (fetchError.value) {
+      console.error("Fetch error:", fetchError.value);
+      error.value = fetchError.value;
+    } else {
+      searchResult.value = data.value;
     }
+  } catch (err) {
+    console.error("An unexpected error occurred:", err);
+    error.value = err;
+  }
   }
   const { data , error } = await useFetch(`${ticketingUrl}/api/v1/getProjects?roleId=${user.value.userRole}`);
 </script>
