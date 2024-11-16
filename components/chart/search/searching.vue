bb2ce92a6ece84bf73983cddd2de92e6c48928df
[Ehsan-Darvishi] <ehsan20031203@gmail.com>
2024-11-16 17:23:24 +0330

    1403-08-26 refactor DataTable data draw - add css to جزئیات buton.

--- components/chart/search/searching.vue
+++ components/chart/search/searching.vue
@@ -1,6 +1,3 @@
-<style>
-
-</style>
 <template>
   <div class="card-body">
     <h6 class="card-title">جستجوی پیشرفته</h6>
@@ -9,13 +6,13 @@
           <div class="col-md-2">
             <div class="form-group">
               <label for="exampleFormControlInput1">شماره تیکت :</label>
-              <input type="text" class="form-control text-left" v-model="query.TicketNumber" placeholder="نمونه شماره تیکت : 1403110001" />
+              <input type="text" class="form-control text-left" v-model="query.TicketNumber" autocomplete="off" placeholder="نمونه شماره تیکت : 1403110001" />
             </div>
           </div>
           <div class="col-md-2">
             <div class="form-group">
               <label for="exampleFormControlInput1">عنوان :</label>
-              <input type="text" class="form-control text-left" v-model="query.Title" placeholder="نمونه عنوان تیکت : قبض"/>
+              <input type="text" class="form-control text-left" v-model="query.Title" autocomplete="off" placeholder="نمونه عنوان تیکت : قبض"/>
             </div>
           </div>
           <div class="col-md-2">
@@ -38,7 +35,7 @@
           <div class="col-md-2">
             <div class="form-group">
               <label for="exampleFormControlInput1">نام کاربری :</label>
-              <input type="text" class="form-control text-left" v-model="query.Username" placeholder="نمونه نام کاربری : admintaz" />
+              <input type="text" class="form-control text-left" v-model="query.Username" autocomplete="off" placeholder="نمونه نام کاربری : admintaz" />
             </div>
           </div>
           <div class="col-md-2">
@@ -91,8 +88,8 @@
                 <option key="0" value="0">همه</option>
               </select>
             </div>
-          </div>
-          <div class="col-md-2">
+        </div>
+        <div class="col-md-2">
             <div class="form-group">
               <label for="exampleFormControlSelect1">نوع درخواست :</label>
               <select class="form-control" v-model="query.RequestType">
@@ -101,8 +98,8 @@
                 <option key="0" value="0">همه</option>
               </select>
             </div>
-          </div>
-          <div class="col-md-2">
+        </div>
+        <div class="col-md-2">
             <div class="form-group">
               <label for="exampleFormControlSelect1">انجام دهنده</label>
               <select v-model="query.DeveloperId" class="form-control">
@@ -118,19 +115,19 @@
                 <option value="0">همه</option>
               </select>
             </div>
-          </div>
-          <div class="col-md-2">
+        </div>
+        <div class="col-md-2">
             <div class="form-group">
               <label for="exampleFormControlSelect1">تاریخ شروع</label>
-              <input type="text" id="startDateTime" name="date-picker-shamsi-list" class="form-control text-left" dir="ltr">
+              <input type="text" id="startDateTime" name="date-picker-shamsi-list" class="form-control text-left" dir="ltr" autocomplete="off">
             </div>
-          </div>
-          <div class="col-md-2">
+        </div>
+        <div class="col-md-2">
             <div class="form-group">
               <label for="exampleFormControlSelect1">تاریخ پایان</label>
-              <input type="text" id="endDateTime" name="date-picker-shamsi-list" class="form-control text-left" dir="ltr">
+              <input type="text" id="endDateTime" name="date-picker-shamsi-list" class="form-control text-left" dir="ltr" autocomplete="off">
             </div>
-          </div>
+        </div>
       </div>
 
       <div class="row">
@@ -143,105 +140,25 @@
   <div class="row">
     <div class="col-md-12">
       <div class="card-body">
-        <table id="search" class="table table-striped table-bordered" width="100%">
-        <thead>
-          <tr>
-            <th>شماره ردیف تیکت</th>
-						<th>کد تیکت</th>
-						<th>ثبت کننده</th>
-						<th>تاریخ ثبت</th>
-						<th>نوع درخواست </th>
-						<th>اولویت</th>
-						<th>عنوان</th>
-						<th>سامانه</th>
-						<th >ایجاد شده توسط</th>
-						<th>ارجاع شده به</th>
-						<th>وضعیت</th>
-						<th>انجام دهنده</th>
-						<th>ساعت صرف شده</th>
-						<th>جزئیات</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in searchResult" :key="id" :name="id">
-            <td>{{item.ticketRowNumber}} </td>
-							<td>{{item.ticketNumber}} </td>
-							<td>{{item.username}} </td>
-							<td>{{item.date}}</td>
-							<td v-if="item.requestType==1">پشتیبانی</td>
-							<td v-else-if="item.requestType==2">توسعه</td>
-							<td v-else></td>
-							<td v-if="item.priority==1">بالا</td>
-							<td v-else-if="item.priority==2">متوسط</td>
-							<td v-else>پایین</td>
-							<td v-if="item.title.length>100">{{item.title.substring(0, 50) + "..."}}</td>
-              <td v-else>{{item.title}}</td>
-							<td>{{item.project}}</td>
-							<td>{{user.userRoleList.find(x => {return x.id == item.insertedRoleId;}).name }}</td>
-              <td>{{user.userRoleList.find(x => {return x.id == item.currentRoleId;}).name }}</td>
-							<td v-if="item.statusId==1">
-								<p class="text-done">{{ item.status }}</p>
-							</td>
-							<td v-else-if="item.statusId==2">
-								<a class="text-inserted">{{ item.status }}</a>
-							</td>
-							<td v-else-if="item.statusId==3">
-								<a class="text-inserted">{{ item.status }}</a>
-							</td>
-							<td v-else-if="item.statusId==4">
-								<a class="text-danger">{{ item.status }}</a>
-							</td>
-							<td v-else-if="item.statusId==5">
-								<a class="text-inserted">{{ item.status }}</a>
-							</td>
-							<td v-else-if="item.statusId==6">
-								<a class="text-inserted">{{ item.status }}</a>
-							</td>
-							<td v-else-if="item.statusId==7">
-								<a class="text-inserted">{{ item.status }}</a>
-							</td>
-							<td v-else-if="item.statusId==8">
-								<a class="text-inserted">{{ item.status }}</a>
-							</td>
-              <td v-else-if="item.statusId==9">
-								<a class="text-awaitingConfirmation">{{ item.status }}</a>
-							</td>
-							<td v-if="item.developerId==1">
-								<p>پویا رضاییه</p>
-							</td>
-							<td v-else-if="item.developerId==2">
-								<p>محمد باقری</p>
-							</td>
-							<td v-else-if="item.developerId==3">
-								<p>توحید حقیقی</p>
-							</td>
-							<td v-else-if="item.developerId==4">
-								<p>مهسا برجی</p>
-							</td>
-							<td v-else-if="item.developerId==5">
-								<p>امیر مسعود صالحی</p>
-							</td>
-							<td v-else-if="item.developerId==6">
-								<p>شکیلا کاظم پور</p>
-							</td>
-							<td v-else-if="item.developerId==7">
-								<p>احسان درویشی</p>
-							</td>
-							<td v-else-if="item.developerId==8">
-								<p>الهه ابراهیمی</p>
-							</td>
-							<td v-else-if="item.developerId==9">
-								<p>ساناز محمد زاده</p>
-							</td>
-							<td v-else-if="item.developerId==10">
-								<p> تخصیص نشده</p>
-							</td>
-							<td>{{item.ticketTime}}</td>
-							<td>
-								<nuxt-link :to="{ path: '/ticket/detail', query: {id: item.id}}">مشاهده</nuxt-link>
-							</td>
-          </tr>
-        </tbody>
+        <table id="searchResult" class="table table-striped table-bordered" width="100%">
+          <thead>
+            <tr>
+              <th>شماره ردیف تیکت</th>
+						  <th>کد تیکت</th>
+						  <th>ثبت کننده</th>
+						  <th>تاریخ ثبت</th>
+						  <th>نوع درخواست </th>
+						  <th>اولویت</th>
+						  <th>عنوان</th>
+						  <th>سامانه</th>
+						  <th >ایجاد شده توسط</th>
+						  <th>ارجاع شده به</th>
+						  <th>وضعیت</th>
+						  <th>انجام دهنده</th>
+						  <th>ساعت صرف شده</th>
+						  <th>جزئیات</th>
+            </tr>
+          </thead>
         </table>
       </div>
     </div>
@@ -253,6 +170,8 @@
   
   const user = useCookie("UserInfo");
   const { public: { ticketingUrl }} = useRuntimeConfig();
+  const { data , error } = await useFetch(`${ticketingUrl}/api/v1/getProjects?roleId=${user.value.userRole}`);
+  const searchResult = reactive([]);
   const query = reactive({
     TicketNumber: "",
     Title: "",
@@ -263,8 +182,8 @@
     ProjectId: 0,
     RequestType: 0,
     DeveloperId: 0,
-});
-  
+  });
+
   onMounted(() => {
     $('input[name="date-picker-shamsi-list"]').datepicker({
       dateFormat: "yy/mm/dd",
@@ -274,10 +193,11 @@
       changeYear: true,
       showButtonPanel: true
     });
-    $('#search').DataTable({
+    $('#searchResult').DataTable({
       responsive: true,
       pageLength: 100,
-      searching: false,
+      // lengthChange: false,
+      // searching: false,
 		  order: [[3, 'desc']],
 		  language: 
       {
@@ -305,7 +225,7 @@
 		  }
     });
   })
-
+  
   function convertShamsiToGregorian(shamsiDate) {
     // فرض کنید تاریخ ورودی به فرمت "yyyy/MM/dd" باشد
     const [year, month, day] = shamsiDate.split('/').map(Number);
@@ -314,31 +234,146 @@
     return `${gy}-${String(gm).padStart(2, '0')}-${String(gd).padStart(2, '0')}`;
   }
 
-  const searchResult = ref([]);
-
-  async function send(){
+  async function send()
+  {
     var StartDateTime = document.getElementById('startDateTime').value;
     var EndDateTime = document.getElementById('endDateTime').value;
     if (!StartDateTime || !EndDateTime) 
     {
-    toastr.error('لطفا تاریخ شروع و پایان را مشخص کنید');
+      toastr.error('لطفا تاریخ شروع و پایان را مشخص کنید');
     }
     try {
-    const { data, error: fetchError } = await useFetch(
+      const { data, error: fetchError } = await useFetch(
       `${ticketingUrl}/api/v1/search?ticketNumber=${query.TicketNumber}&title=${query.Title}&insertedRoleId=${query.InsertedRoleId}&username=${query.Username}&CurrentRoleId=${query.CurrentRoleId}&statusId=${query.StatusId}&projectId=${query.ProjectId}&requestType=${query.RequestType}&developerId=${query.DeveloperId}&startDateTime=${convertShamsiToGregorian(StartDateTime)}&endDateTime=${convertShamsiToGregorian(EndDateTime)}`
-    );
+      );
 
-    if (fetchError.value) {
-      console.error("Fetch error:", fetchError.value);
-      error.value = fetchError.value;
-    } else {
-      searchResult.value = data.value;
+      if (!fetchError.value) //fetchError.value == null or empty
+      {
+        searchResult.value = data.value;
+        
+        // Destroy existing DataTable instance
+        $('#searchResult').DataTable().destroy();
+        // Reinitialize DataTable with updated data
+        $('#searchResult').DataTable({
+          data: searchResult.value,
+          responsive: true,
+          pageLength: 100,
+          // lengthChange: false,
+          //searching: false,
+          order: [[3, 'desc']],
+          columns: [
+            { data: 'ticketRowNumber' },
+            { data: 'ticketNumber' },
+            { data: 'username' },
+            { data: 'date' },
+            { 
+              data: 'requestType',
+              render: (data) => (data === 1 ? 'پشتیبانی' : (data === 2 ? 'توسعه' : ''))
+            },
+            { 
+              data: 'priority',
+              render: (data) => (data === 1 ? 'بالا' : (data === 2 ? 'متوسط' : 'پایین'))
+            },
+            { 
+              data: 'title',
+              render: (data) => (data.length > 100 ? data.substring(0, 50) + "..." : data)
+            },
+            { data: 'project' },
+            { 
+              data: 'insertedRoleId',
+              render: (data) => user.value.userRoleList.find(x => x.id == data)?.name || ''
+            },
+            { 
+              data: 'currentRoleId',
+              render: (data) => user.value.userRoleList.find(x => x.id == data)?.name || ''
+            },
+            { 
+              data: 'statusId',
+              render: (data, type, row) => {
+                let statusClass = '';
+                let statusText = '';
+                switch (data) 
+                {
+                  case 1:
+                    statusClass = 'text-done';
+                    statusText = row.status;
+                    break;
+                  case 2:
+                  case 3:
+                  case 5:
+                  case 6:
+                  case 7:
+                  case 8:
+                    statusClass = 'text-inserted';
+                    statusText = row.status;
+                    break;
+                  case 4:
+                    statusClass = 'text-danger';
+                    statusText = row.status;
+                    break;
+                  case 9:
+                    statusClass = 'text-awaitingConfirmation';
+                    statusText = row.status;
+                    break;
+                }
+                return `<a class="${statusClass}">${statusText}</a>`;
+              }
+            },
+            { 
+              data: 'developerId',
+              render: (data) => {
+                const developers = {
+                  1: 'پویا رضاییه',
+                  2: 'محمد باقری',
+                  3: 'توحید حقیقی',
+                  4: 'مهسا برجی',
+                  5: 'امیر مسعود صالحی',
+                  6: 'شکیلا کاظم پور',
+                  7: 'احسان درویشی',
+                  8: 'الهه ابراهیمی',
+                  9: 'ساناز محمد زاده',
+                  10: 'تخصیص نشده'
+                };
+                return developers[data] || '';
+              }
+            },
+            { data: 'ticketTime' },
+            {
+              data: 'id',
+              render: (id) => {
+                return `<a href="/ticket/detail?id=${id}" class="custom-link">مشاهده</a>`;
+              }
+            },
+          ],
+          language: {
+            sEmptyTable: "هیچ داده ای در جدول وجود ندارد",
+            sInfo: "نمایش _START_ تا _END_ از _TOTAL_ رکورد",
+            sInfoEmpty: "نمایش 0 تا 0 از 0 رکورد",
+            sInfoFiltered: "(فیلتر شده از _MAX_ رکورد)",
+            sLoadingRecords: "در حال بارگزاری...",
+            sProcessing: "در حال پردازش...",
+            sSearch: "جستجو:",
+            sZeroRecords: "رکوردی با این مشخصات پیدا نشد",
+            oPaginate: {
+              sFirst: "ابتدا",
+              sLast: "انتها",
+              sNext: "بعدی",
+              sPrevious: "قبلی",
+            },
+          },
+        });
+      } 
+      else 
+      {
+        console.error("Fetch error:", fetchError.value);
+        error.value = fetchError.value;
+      }
+    } 
+    catch (err) {
+      console.error("An unexpected error occurred:", err);
     }
-  } catch (err) {
-    console.error("An unexpected error occurred:", err);
-    error.value = err;
   }
-  }
-  const { data , error } = await useFetch(`${ticketingUrl}/api/v1/getProjects?roleId=${user.value.userRole}`);
+
+  
 </script>
   
