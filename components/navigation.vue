1e310c4be6777932337581cd52ddd502c8d90d05
[Ehsan-Darvishi] <ehsan20031203@gmail.com>
2024-12-16 17:11:18 +0330

    1403/09/26

--- components/navigation.vue
+++ components/navigation.vue
@@ -31,6 +31,9 @@
             <li v-if="user.userRole==5">
                 <NuxtLink class="active" to="/search/searching">جستجوی پیشرفته<span class="badge badge-danger">2</span></NuxtLink>
             </li>
+            <li v-if="user.userRole==5">
+                <NuxtLink class="active" to="/dashboard/dashboard">داشبورد<span class="badge badge-primary">2</span></NuxtLink>
+            </li>
         </ul>
     </div>
 </div>
---
+++ pages/dashboard/dashboard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useRequestTypeStore } from '@/stores/requestTypeStore'
+import Dashboard from '~/components/dashboard/dashboard.vue';
+const changeRequestTypeId = useRequestTypeStore();
+changeRequestTypeId.showSwitchButton(false);
+definePageMeta({
+  layout: 'panel'
+})
+</script>
+
+<template>
+    <div class="row">
+			<div class="col-md-12">
+				<div class="card">
+					<Dashboard/>
+				</div>
+			</div>
+		</div>
+</template>
